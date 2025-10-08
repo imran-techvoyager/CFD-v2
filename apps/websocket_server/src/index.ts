67ee44be01cfe4464d1b0e2009930db5c9d3fc80
Imran Ahmed <imran.techvoyager@gmail.com>
2025-10-08 10:09:44 +0530

    refactor(websocket_server): unify client map variable naming fix: correct casing of project name in package.json

--- apps/websocket_server/src/index.ts
+++ apps/websocket_server/src/index.ts
@@ -2,7 +2,7 @@ import WebSocket, { WebSocketServer } from "ws";
 import { redis } from "@repo/redis/client";
 
 const subClient = redis.duplicate();
-const client = new Map<WebSocket, Set<string>>();
+const clients = new Map<WebSocket, Set<string>>();
 const channels = ["BTC", "ETH", "SOL"];
 
 const wss = new WebSocketServer({ port: 8080 });
@@ -10,35 +10,35 @@ const wss = new WebSocketServer({ port: 8080 });
 async function start() {
   await subClient.connect();
 
-  channels.forEach((ch) => {
-    redis.subscribe(ch, (msg) => {
-      client.forEach((symbs, ws: WebSocket) => {
-        if (symbs.has(ch)) {
-          ws.send(msg);
+  await subClient.subscribe(...channels);
+
+  subClient.on("message", (channel, message) => {
+    clients.forEach((symbs, ws: WebSocket) => {
+        if(symbs.has(channel)){
+            ws.send(message);
         }
-      });
     });
   });
 
   wss.on("connection", (socket: WebSocket) => {
-    client.set(socket, new Set());
+    clients.set(socket, new Set());
 
     socket.on("message", (msg) => {
       const message = JSON.parse(msg.toString());
       if (message.type === "SUBSCRIBE") {
-        if (!client.has(socket)) {
-          client.set(socket, new Set());
+        if (!clients.has(socket)) {
+          clients.set(socket, new Set());
         }
 
-        const symbs = client.get(socket);
+        const symbs = clients.get(socket);
         symbs?.add(message.symbol);
       }
 
       if (message.type === "UNSUBSCRIBE") {
-        const symbs = client.get(socket);
+        const symbs = clients.get(socket);
         symbs?.delete(message.symbol);
         if (symbs?.size === 0) {
-          client.delete(socket);
+          clients.delete(socket);
         }
       }
     });
